70c8ee6f9fddc49bd139b4b757aa3e65b92caef0
Ch4oooooooLL <lyc@cqu.edu.cn>
2026-03-17 00:13:13 +0800

    feat: Add IRoutingDevice interface for routing device connection detection
    - Create DeviceType enum with CONTROLLER, TERMINAL, CABLE types
    - Add canConnectFrom() method for directional connection support
    - Marker interface for routing cable network discovery

---
+++ src/main/java/com/ae2channelrouter/api/IRoutingDevice.java
@@ -0,0 +1,38 @@
+package com.ae2channelrouter.api;
+
+import net.minecraftforge.common.util.ForgeDirection;
+
+/**
+ * Interface for routing devices that can connect to routing cables.
+ * 
+ * Implement this interface on TileEntities that should be discoverable
+ * and connectable by routing cables.
+ */
+public interface IRoutingDevice {
+    
+    /**
+     * Get the type of routing device.
+     * 
+     * @return Device type identifier
+     */
+    DeviceType getDeviceType();
+    
+    /**
+     * Check if this device can accept a connection from a specific direction.
+     * 
+     * @param from The facing direction of the incoming connection
+     * @return true if connection is allowed
+     */
+    default boolean canConnectFrom(ForgeDirection from) {
+        return true;
+    }
+    
+    /**
+     * Types of routing devices.
+     */
+    enum DeviceType {
+        CONTROLLER,
+        TERMINAL,
+        CABLE
+    }
+}
